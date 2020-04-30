cacd85326b218585f2d9581f59152d1358efe3e3
kobbi1 <jakob-daniel@hotmail.com>
2020-04-30 23:35:22 +0000

    Update Utils.cs

--- ModAssistant/Classes/Utils.cs
+++ ModAssistant/Classes/Utils.cs
@@ -349,7 +349,7 @@ namespace ModAssistant
                 File.Exists(Path.Combine(directory, "Beat Saber_Data", "Plugins", "BSteam crack.dll")) ||
                 File.Exists(Path.Combine(directory, "Beat Saber_Data", "Plugins", "HUHUVR_steam_api64.dll")) ||
                 Directory.GetFiles(Path.Combine(directory, "Beat Saber_Data", "Plugins"), "*.ini", SearchOption.TopDirectoryOnly).Length > 0)
-                return true;
+                return false;
             return false;
         }
 
